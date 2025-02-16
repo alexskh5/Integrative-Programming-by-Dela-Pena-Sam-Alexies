995bd4464beaacf1b3ceac2a1117b3893f5410c8
Sam Alexies H. Dela Peña <samalexies.delapena@ctu.edu.ph>
2025-02-15 23:43:28 -0600

    Update weather_conditions.php

--- delapena_exercise1_php/weather_conditions.php
+++ delapena_exercise1_php/weather_conditions.php
@@ -4,40 +4,8 @@
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <link rel="stylesheet" href="weather_conditions.css">
         <title>Weather Conditions</title>
-        <style>
-            body {
-                font-family: 'Poppins', sans-serif;
-                margin: 0;
-                height: 100vh;
-                background: url('sunset.gif') no-repeat center center/cover;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                text-align: center;
-                color: white;
-                background-attachment: fixed;
-            }
-            .content {
-                background-color: rgba(43, 44, 44, 0.7);
-                padding: 30px;
-                border-radius: 15px;
-                font-size: 20px;
-                line-height: 1.8;
-                max-width: 600px;
-                box-shadow: 0 4px 10px rgba(61, 59, 59, 0.5);
-            }
-            h1 {
-                margin-bottom: 20px;
-                font-size: 36px;
-                text-transform: uppercase;
-                letter-spacing: 2px;
-                color: #FFD700;
-            }
-            p {
-                margin: 10px 0;
-            }
-        </style>
     </head>
     <body>
         <div class="content">
